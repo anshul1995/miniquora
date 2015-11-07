feb28c456732462ebebe0f29c46638a57de7a20e
Anushray Gupta <anushray7@gmail.com>
2015-11-07 12:37:17 +0530

    Signup Complete

--- miniquora/account/urls.py
+++ miniquora/account/urls.py
@@ -3,4 +3,5 @@ urlpatterns = [
     url(r'^logout/$', 'account.views.logout', name = 'logout'),
     url(r'^home/$', 'account.views.home',name='home'),
     url(r'^signup/$', 'account.views.signup',name='signup'),
+    url(r'^activate/(?P<uid>\d+)/(?P<token>[0-9A-Za-z_\-]+)/$', 'account.views.activate', name='activate'),
 ]
--- miniquora/account/views.py
+++ miniquora/account/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.core.exceptions import SuspiciousOperation
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
 from django.contrib.auth.decorators import login_required
+from django.core.mail import EmailMultiAlternatives
+from django.contrib.sites.shortcuts import get_current_site
+from django.conf import settings
+from django.template import loader
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .forms import LoginForm, SignupForm
 from .models import CustomUser
 
@@ -33,7 +39,16 @@ def signup(request):
         f = SignupForm(request.POST)
         if f.is_valid():
             user = f.save();
-            #send verification email as well.
+            email_body_context = {
+                'username' : user.username,
+                'token': urlsafe_base64_encode(force_bytes(user.username)),
+                'uid' : user.id,
+                'protocol': 'https' if settings.USE_HTTPS else 'http',
+                'domain' : get_current_site(request).domain
+            }
+            body = loader.render_to_string('authentication/signup_email_body_text.html', email_body_context)
+            email_message = EmailMultiAlternatives('Welcome To MiniQuora',body, settings.DEFAULT_FROM_EMAIL, [user.email])
+            email_message.send()
             return render(request, 'authentication/signup_email_sent.html', { 'email' : user.email })
     return render(request, 'authentication/signup.html', { 'form': f})
 
@@ -47,4 +62,25 @@ def logout(request):
 def home(request):
     return render(request, 'base/loggedin.html');
 
-    
+
+@require_GET
+def activate(request, uid = None, token = None):
+    if request.user.is_authenticated():
+        return redirect('home')
+    '''
+    try:
+        user = CustomUser.objects.get(id = uid)
+    except CustomUser.DoesNotExist:
+        raise Http404('Invalid User')
+    '''
+    user = get_object_or_404(CustomUser, id = uid)
+    username_from_token = force_text(urlsafe_base64_decode(token))
+    if user.is_active:
+        return redirect('base')
+
+    if user.username == username_from_token:
+        user.is_active = True
+        user.save()
+        return render(request, 'authentication/activation_success.html')
+    else:
+        return render(request, 'authentication/activation_failure.html')
